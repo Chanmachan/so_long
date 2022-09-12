bcb04a92572721214d020b189caf03f1349f37c5
chanma <yunosuke.homma@icloud.com>
2022-09-12 16:33:12 +0900

    add gnl

--- includes/so_long.h
+++ includes/so_long.h
@@ -1,8 +1,8 @@
-//
-// Created by 本間優之介 on 2022/09/12.
-//
-
 #ifndef SO_LONG_SO_LONG_H
-#define SO_LONG_SO_LONG_H
+# define SO_LONG_SO_LONG_H
 
-#endif //SO_LONG_SO_LONG_H
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
