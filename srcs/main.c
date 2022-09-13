8a7f749b8a2e261ca3d893451d4562a5b41f3d59
chanma <yunosuke.homma@icloud.com>
2022-09-14 00:31:24 +0900

    load_file

--- srcs/main.c
+++ srcs/main.c
@@ -1,25 +1,46 @@
 #include "../includes/so_long.h"
 
+//int	valid_map()
+
+void	load_map()
+{
+	char	**str;
+	int 	fd;
+	char	**tmp;
+	char	*tmp2;
+
+	fd = open("maps/sample.ber", O_RDONLY);
+	tmp = str;
+	while (1)
+	{
+		str = (char **) malloc(sizeof(char) * 1);
+		if (str == NULL)
+			exit(EXIT_FAILURE);
+		*str = get_next_line(fd);
+		printf("%s", *str);
+		tmp2 = *str;
+		tmp = str;
+		if (*str == NULL)
+		{
+			free(tmp);
+			break;
+		}
+		str++;
+		free(tmp2);
+		free(tmp);
+	}
+	close(fd);
+}
+
 int	main(void)
 {
 //	size_t	i;
-	char	*str;
-	int		fd;
+//	size_t	row;//行(横)
+//	size_t	column;//列(縦)
 
-	fd = open("../maps/sample.ber", O_RDONLY);
-	printf("%d\n", fd);
-//	i = 0;
-	while (1)
-	{
-		str = get_next_line(fd);
-		printf("%s\n", str);
-		if (str == NULL)
-		{
-			free(str);
-			break;
-		}
-		free(str);
-	}
-	close(fd);
+//	row = 0;
+//	column = 0;
+	load_map();
+	system("leaks -q so_long");
 	return (0);
 }
