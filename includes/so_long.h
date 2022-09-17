fa4316f00d163c4e13d1cb2d39d7844444bd74bd
chanma <yunosuke.homma@icloud.com>
2022-09-17 17:54:02 +0900

    add typedef

--- includes/so_long.h
+++ includes/so_long.h
@@ -8,12 +8,19 @@
 # include "../libft/includes/libft.h"
 # include "../minilibx-linux/mlx.h"
 
-typedef struct s_info
+typedef struct s_map_info
 {
+	char	**map;
 	size_t	row;
 	size_t	column;
-	char	**map;
-//	size_t
+}				t_map_info;
+
+typedef struct s_info
+{
+	void		*mlx_id;
+	void		*mlx_win_id;
+	t_map_info	*map_info;
+	//	size_t
 } 				t_info;
 
 #endif
