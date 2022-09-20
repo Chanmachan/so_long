820359c41a6892da55126e1921ab6ea8e36ce087
chanma <yunosuke.homma@icloud.com>
2022-09-20 17:44:58 +0900

    add define

--- includes/so_long.h
+++ includes/so_long.h
@@ -8,6 +8,21 @@
 # include "../libft/includes/libft.h"
 # include "../minilibx-linux/mlx.h"
 
+# define EXIT_CLOSE "imgs/exit/map_exit_close.xpm"
+# define EXIT_OPEN "imgs/exit/map_exit_open.xpm"
+# define P_BACK_1 "imgs/player/map_person_back_1.xpm"
+# define P_BACK_2 "imgs/player/map_person_back_2.xpm"
+# define P_BACK_3 "imgs/player/map_person_back_3.xpm"
+# define P_FRONT_1 "imgs/player/map_person_front_1.xpm"
+# define P_FRONT_2 = "imgs/player/map_person_front_2.xpm"
+# define P_FRONT_3 = "imgs/player/map_person_front_3.xpm"
+# define P_LEFT_1 "imgs/player/map_person_left_1.xpm"
+# define P_LEFT_2 "imgs/player/map_person_left_2.xpm"
+# define P_LEFT_3 "imgs/player/map_person_left_3.xpm"
+# define P_RIGHT_1 "imgs/player/map_person_right_1.xpm"
+# define P_RIGHT_2 "imgs/player/map_person_right_2.xpm"
+# define P_RIGHT_3 "imgs/player/map_person_right_3.xpm"
+
 typedef struct s_map_info
 {
 	char	**map;
